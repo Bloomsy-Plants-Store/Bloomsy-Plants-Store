154bf6257394f34e12275201cd93c65d1c3f46e2
Mariam-Mokhtar <mariamreda1819@gmail.com>
2023-05-03 00:39:55 +0300

    Footer Component

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { SampleProductsComponent } from './Components/Pages/Home/sample-products/sample-products.component';
 import { HomePageComponent } from './Components/Pages/Home/home-page/home-page.component';
 import { ReviewComponent } from './Components/Pages/Home/review/review.component';
+import { FooterComponent } from './Components/Pages/Home/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SampleProductsComponent,
     HomePageComponent,
-    ReviewComponent
+    ReviewComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
