7955b909b15df56dfe0c84a908b7c0b2304e7f36
Omnia-Goher <omnia.goher9@gmail.com>
2023-05-02 22:21:24 +0300

    header

--- FrontEnd/src/app/Components/Pages/Home/header/header.component.ts
+++ FrontEnd/src/app/Components/Pages/Home/header/header.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+
+import { Component, ViewChild, ElementRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
 export class HeaderComponent {
 
+  constructor() {
+    
+  }
 }
