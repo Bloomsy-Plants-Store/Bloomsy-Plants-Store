f55b44a9d140a74edd3b333418e846f78360d7f7
Omnia-Goher <omnia.goher9@gmail.com>
2023-05-02 15:17:23 +0300

    create header component

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SampleProductsComponent } from './Components/Pages/Home/sample-products/sample-products.component';
 import { HomepageComponent } from './Components/Pages/Home/homepage/homepage.component';
+import { HeaderComponent } from './Components/Pages/Home/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SampleProductsComponent,
-    HomepageComponent
+    HomepageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
