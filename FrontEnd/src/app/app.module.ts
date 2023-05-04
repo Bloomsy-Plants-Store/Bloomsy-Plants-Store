42fb1b0b6dfb50d5788469b41d1e785d2ea41b4d
Radwa99 <radwa.prof.9@gmail.com>
2023-05-04 14:13:23 +0300

    edits

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { HomePageComponent } from './Components/Pages/Home/home-page/home-page.c
 import { AboutComponent } from './Components/Pages/Home/about/about.component';
 import { NavbarComponent } from './Components/Pages/Home/navbar/navbar.component';
 import { HeaderComponent } from './Components/Pages/Home/header/header.component';
-// import { SampleProductsComponent } from './Components/Pages/Home/sample-products/sample-products.component';
+import { SampleProductsComponent } from './Components/Pages/Home/sample-products/sample-products.component';
 import { ReviewComponent } from './Components/Pages/Home/review/review.component';
 import { FooterComponent } from './Components/Pages/Home/footer/footer.component';
 import { SupportComponent } from './Components/Pages/Home/support/support.component';
@@ -18,7 +18,7 @@ import { BannersComponent } from './Components/Pages/Home/banners/banners.compon
 @NgModule({
   declarations: [
     AppComponent,
-    // SampleProductsComponent,
+    SampleProductsComponent,
     HomePageComponent,
     HeaderComponent,
     ReviewComponent,
