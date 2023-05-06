c237274d5be2e74f606244b13aab6fb6e4b961c2
Hager Abd El Galil <Hager.galil66@gmail.com>
2023-05-06 10:17:07 +0300

    Add Contact Us Routes

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactUsPageComponent } from './Components/Pages/Contact Us/contact-us-page/contact-us-page.component';
 import { HomePageComponent } from './Components/Pages/Home/home-page/home-page.component';
 
 
 const routes: Routes = [
   {path:'',component:HomePageComponent},
+  {path:'contact-us',component:ContactUsPageComponent},
 ];
 
 @NgModule({
