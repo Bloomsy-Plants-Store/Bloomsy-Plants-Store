1dc4321b7ab1fd786048dcabe6b99381546787ae
Hager Abd El Galil <Hager.galil66@gmail.com>
2023-05-06 10:03:15 +0300

    Update Files Structure

---
+++ FrontEnd/src/app/Components/Pages/Contact Us/contact-us-page/contact-us-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-us-page',
+  templateUrl: './contact-us-page.component.html',
+  styleUrls: ['./contact-us-page.component.css']
+})
+export class ContactUsPageComponent {
+
+}
