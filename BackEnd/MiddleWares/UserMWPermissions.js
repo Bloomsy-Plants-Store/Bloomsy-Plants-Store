1b07fc322a0e6e6a97a009361642b7ba7fb2906d
Radwa99 <radwa.prof.9@gmail.com>
2023-05-07 21:09:37 +0300

    UserMWPermission

---
+++ BackEnd/MiddleWares/UserMWPermissions.js
@@ -0,0 +1,23 @@
+var jwt = require("jsonwebtoken");
+
+module.exports = (req,res,next)=>{
+    var Token = req.header("x-auth-token");
+    if(!Token) return res.status(400).send("Access Denied...");
+    
+    if(isAdmin(Token)){
+        next(); 
+    }else{
+        res.status(400).send("Forbidden Access...");
+    }
+
+}
+
+function isAdmin(userToken){
+    var decodePayload = jwt.verify(userToken,"thisistokensecret");
+    if(decodePayload.adminRole){
+        return true;
+    }else{
+        return false;
+    }
+
+}
