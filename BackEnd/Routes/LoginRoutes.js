b293bfd321d2a7c6eba96e8a37c8bf7f4e0c0ff7
Hager Abd El Galil <Hager.galil66@gmail.com>
2023-05-07 21:24:59 +0300

    Add Login Routes

---
+++ BackEnd/Routes/LoginRoutes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = new express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const userModel = require("../Models/UsersModel");
+const loginValid = require("../Utils/LoginValidate");
+
+router.post("/",async(req,res)=>{
+    //Check Email
+    var user = await userModel.findOne({email:req.body.email}).exec();
+    if(!user) return res.status(400).send("Invalid Email Or Password")
+    
+    //Check Password
+    var checkpass = await bcrypt.compare(req.body.password, user.password).exec();
+    if(!checkpass) return res.status(400).send("Invalid Email Or Password")
+    
+    //Login
+    res.status(200).send("Logged In Successfully!")
+
+})
+
+
