d5ad0d9ba8f80f6fbb9cdd7085d5732abfde80fb
Hager Abd El Galil <Hager.galil66@gmail.com>
2023-05-07 23:54:50 +0300

    Update Login Validator

--- BackEnd/Utils/LoginValidate.js
+++ BackEnd/Utils/LoginValidate.js
@@ -5,14 +5,12 @@ userSchema = {
     "properties":{
         email: {
             type: "string",
-            pattern: "^[a-zA-Z0-9]+@[a-zA-Z0-9]+(.com){1}$",
-            errorMessage: "Invalid email address."
-          },
-          password: {
-            type: "string",
-            pattern: "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
-            errorMessage: "Password should contain at least 8 characters, including at least one uppercase letter, one lowercase letter, one digit, and one special character."
+            pattern: "^[a-zA-Z0-9]+@[a-zA-Z0-9]+(.com){1}$"
         },
+        password: {
+            type: "string",
+            pattern: "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d$!%*#?&@]{8,}$"
+        }
     },
     "required":["email","password"]
 }
