da570c3f129fb30203dc8a0629eba7abfd0eec5e
Hager Abd El Galil <Hager.galil66@gmail.com>
2023-05-07 22:31:35 +0300

    Login Validator

--- BackEnd/Utils/LoginValidate.js
+++ BackEnd/Utils/LoginValidate.js
@@ -3,11 +3,18 @@ var ajv = new Ajv();
 userSchema = {
     "type":"object",
     "properties":{
-        "email":{"type":"string","pattern":"^[a-zA-Z0-9]+\@{1}[a-zA-Z0-9]+(.com){1}$"},
-        "password":{"type":"string","pattern":"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"}
+        email: {
+            type: "string",
+            pattern: "^[a-zA-Z0-9]+@[a-zA-Z0-9]+(.com){1}$",
+            errorMessage: "Invalid email address."
+          },
+          password: {
+            type: "string",
+            pattern: "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            errorMessage: "Password should contain at least 8 characters, including at least one uppercase letter, one lowercase letter, one digit, and one special character."
+        },
     },
     "required":["email","password"]
 }
-
 
 module.exports = ajv.compile(userSchema);
