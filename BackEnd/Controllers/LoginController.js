5dc44b10d8b777831f66ab04fa57503185ad48c3
Hager Abd El Galil <Hager.galil66@gmail.com>
2023-05-07 21:58:09 +0300

    Login Controller

---
+++ BackEnd/Controllers/LoginController.js
@@ -0,0 +1,24 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const userModel = require("../Models/UsersModel");
+const loginValid = require("../Utils/LoginValidate");
+
+var Login = async(req,res)=>{
+
+    //Check Email
+    var user = await userModel.findOne({email:req.body.email}).exec();
+    if(!user) return res.status(400).send("Invalid Email Or Password")
+    
+    //Check Password
+    var checkpass = await bcrypt.compare(req.body.password, user.password).exec();
+    if(!checkpass) return res.status(400).send("Invalid Email Or Password")
+    
+    //Login
+    var Token = jwt.sign({UserId:user._id},"thisistokensecret")
+    res.header("x-auth-token",Token);
+    res.status(200).send("Logged In Successfully!")
+
+}
+
+module.exports = Login;
--- BackEnd/Routes/LoginRoutes.js
+++ BackEnd/Routes/LoginRoutes.js
@@ -1,26 +1,7 @@
 const express = require("express");
 const router = new express.Router();
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const LoginController = require("../Controllers/LoginController")
 
-const userModel = require("../Models/UsersModel");
-const loginValid = require("../Utils/LoginValidate");
-
-router.post("/",async(req,res)=>{
-
-    //Check Email
-    var user = await userModel.findOne({email:req.body.email}).exec();
-    if(!user) return res.status(400).send("Invalid Email Or Password")
-    
-    //Check Password
-    var checkpass = await bcrypt.compare(req.body.password, user.password).exec();
-    if(!checkpass) return res.status(400).send("Invalid Email Or Password")
-    
-    //Login
-    var Token = jwt.sign({UserId:user._id},"thisistokensecret")
-    res.header("x-auth-token",Token);
-    res.status(200).send("Logged In Successfully!")
-
-})
+router.post("/", LoginController)
 
 module.exports = router;
