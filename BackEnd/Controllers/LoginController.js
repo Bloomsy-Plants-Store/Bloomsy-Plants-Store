d23a0dbe8e6332095633beb7b36cd1fff2d817dc
Hager Abd El Galil <Hager.galil66@gmail.com>
2023-05-09 02:10:04 +0300

    Remember Me

--- BackEnd/Controllers/LoginController.js
+++ BackEnd/Controllers/LoginController.js
@@ -6,19 +6,28 @@ const userModel = require("../Models/UsersModel");
 const loginValid = require("../Utils/LoginValidate");
 
 var Login = async(req,res)=>{
-    //Check Email
-    var user = await userModel.findOne({email:req.body.email}).exec();
-    if(!user) return res.status(400).send("Invalid Email Or Password")
-    
-    //Check Password
-    var checkpass = await bcrypt.compare(req.body.password, user.password);
-    if(!checkpass) return res.status(400).send("Invalid Email Or Password")
-    
-    //Login
-    var Token = jwt.sign({UserId:user._id},config.SECRETKEY)
-    res.header("x-auth-token",Token);
-    res.status(200).send("Logged In Successfully!")
+    // return passport.authenticate('local', { failureRedirect: '/login' })(req, res, async () => {
+    //     if (req.body.rememberMe) {
+    //       const token = await req.user.generateRememberMeToken();
+    //       await User.updateOne({ _id: req.user._id }, { $set: { rememberMeToken: token } });
+    //       res.cookie('remember_me', token, { maxAge: 604800000 }); // Expires after 7 days
+    //     }else{
+            //Check Email
+            var user = await userModel.findOne({email:req.body.email}).exec();
+            if(!user) return res.status(400).send("Invalid Email Or Password")
 
-}
+            //Check Password
+            var checkpass = await bcrypt.compare(req.body.password, user.password);
+            if(!checkpass) return res.status(400).send("Invalid Email Or Password")
+
+            //Login
+            var Token = jwt.sign({UserId:user._id},config.SECRETKEY)
+            res.header("x-auth-token",Token);
+            res.status(200).send("Logged In Successfully!")
+        }
+    
+    //   });
+    
+// }
 
 module.exports = {Login};
