1559b94b4cc455729513b33561db4d19464b4d0c
rowan <rowantamer5@gmail.com>
2023-05-09 00:44:47 +0300

    login with google

---
+++ BackEnd/Routes/GoogleRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = new express.Router();
+const GoogleController = require("../Controllers/GoogleController");
+
+
+
+router.get('/', GoogleController.LoginWithGoogle);
+router.get('/callback', GoogleController.handleLoginWithGoogleCallback);
+
+module.exports = router;
