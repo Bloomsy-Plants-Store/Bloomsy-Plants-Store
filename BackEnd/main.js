31eefe501ee841d2a5c1df797a8b6e36bf1f8d3a
Radwa99 <radwa.prof.9@gmail.com>
2023-05-10 20:33:40 +0300

    hash password model middleware

--- BackEnd/main.js
+++ BackEnd/main.js
@@ -12,22 +12,12 @@ const bodyparser = require("body-parser");
 app.use(bodyparser.urlencoded({extended:true}));
 app.use(bodyparser.json());
 
-
-// Enable CORS for a specific origin
-// Set up CORS middleware
-// app.use(
-//   cors({
-//     origin: 'http://localhost:4200', // Replace with your frontend URL
-//     optionsSuccessStatus: 200, // Some legacy browsers (e.g., IE11) choke on 204
-//     exposedHeaders: ['x-auth-token'] 
-//   })
-// );
 const corsOptions = {
-  origin: 'http://localhost:4200', // Replace with your allowed origin
+  origin: 'http://localhost:4200', //allowed origin
   optionsSuccessStatus: 200,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Replace with your allowed methods
-  allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token'], // Replace with your allowed headers
-  exposedHeaders: ['x-auth-token'] // Replace with your exposed headers
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], //allowed methods
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token'], //allowed headers
+  exposedHeaders: ['x-auth-token'] //exposed headers
 };
 
 app.use(cors(corsOptions));
@@ -44,14 +34,6 @@ app.use(passport.session());
 //Global MiddleWare
 const logging = require("./MiddleWares/Logging");
 app.use("/",logging);
-
-// //CORS MiddleWare
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-//   next();
-// });
 
 // Register Routes
 const UserRoutes = require("./Routes/UsersRoutes");
