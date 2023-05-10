4b02718f8c60aba56c599f444c41f3b57c398d2d
Mariam-Mokhtar <mariamreda1819@gmail.com>
2023-05-10 23:53:28 +0300

    Product Service

--- FrontEnd/src/app/Services/products.service.ts
+++ FrontEnd/src/app/Services/products.service.ts
@@ -6,11 +6,11 @@ import { Injectable } from '@angular/core';
 })
 export class ProductsService {
 
-  private Base_URL = "http://localhost:3000/products";
+  private Base_URL = "http://localhost:7400/api/products/";
 
   constructor(private readonly myClient : HttpClient) { }
 
-  GetAllProducts(){
-    return this.myClient.get(this.Base_URL);
+  GetTopRating(){
+    return this.myClient.get(this.Base_URL+"topRating?limit=6");
   }
 }
