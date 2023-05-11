eb81a6eed60c158df18572145e184f9d6d8f99b1
rowan <rowantamer5@gmail.com>
2023-05-12 00:24:32 +0300

    remember me

--- BackEnd/Controllers/LoginController.js
+++ BackEnd/Controllers/LoginController.js
@@ -2,34 +2,35 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const config = require("../config.json");
 const passport = require('passport');
-const userModel = require("../Models/UsersModel");
+// const userModel = require("../Models/UsersModel");
 const User = require("../Models/UsersModel");
 const rememberMeToken = require("../Utils/rememberMeToken");
+const RememberMeStrategy = require('passport-remember-me').Strategy;
+
+
 
 
 var Login = async (req, res) => {
   //Check Email
-  var user = await userModel.findOne({ email: req.body.email }).exec();
+  var user = await User.findOne({ email: req.body.email }).exec();
   if (!user) return res.status(400).json({ message: 'Invalid Email Or Password' });
 
   //Check Password
   var checkpass = await bcrypt.compare(req.body.password, user.password);
   if (!checkpass) return res.status(400).json({ message: 'Invalid Email Or Password' });
 
-  return passport.authenticate('local', { failureRedirect: '/login' })
-    (req, res, async () => {
-      
-      if (req.body.rememberMe) {
+  return passport.authenticate('local', { failureRedirect: '/login' })(req, res, async () => {
+    if (req.body.rememberMe) {
+      if (!user.rememberMeToken) {
         const token = await rememberMeToken;
         await User.updateOne({ email: req.body.email }, { $set: { rememberMeToken: token } });
         res.cookie('remember_me', token, { maxAge: 604800000 }); // Expires after 7 days
       }
-
-
-      //Login
-      var Token = jwt.sign({ UserId: user._id, UserName: user.name }, config.SECRETKEY)
-      res.header({ "x-auth-token": Token });
-      res.status(200).json({ message: "Logged In Successfully!" })
+    }
+    //Login
+    var Token = jwt.sign({ UserId: user._id, UserName: user.name }, config.SECRETKEY)
+    res.header({ "x-auth-token": Token });
+    res.status(200).json({ message: "Logged In Successfully!" })
 
     });
 
--- BackEnd/Controllers/RememberMeController.js
+++ BackEnd/Controllers/RememberMeController.js
@@ -1,23 +1,23 @@
-const passport = require('passport');
-const RememberMeStrategy = require('passport-remember-me').Strategy;
-const User = require('../Models/UsersModel');
-const rememberMeToken = require("../Utils/rememberMeToken");
+// const passport = require('passport');
+// const RememberMeStrategy = require('passport-remember-me').Strategy;
+// const User = require('../Models/UsersModel');
+// // const rememberMeToken = require("../Utils/rememberMeToken");
 
-passport.use(new RememberMeStrategy(
-  
-  async (token, done) => {
-    const user = await User.findOne({ rememberMeToken: token });
+// passport.use(new RememberMeStrategy(
+//   async (token, done) => {
+//     const user = await User.findOne({ rememberMeToken: token });
 
-    if (!user) {
-      return done(null, false);
-    }
+//     if (!user) {
+//       return done(null, false);
+//     }
 
-    return done(null, user);
-  },
-  async (user, done) => {
-    const token = await user.rememberMeToken;
-    await User.updateOne({ email: req.body.email }, { $set: { rememberMeToken: token } });
-    return done(null, token);
-  }
-));
-// module.exports = {}
+//     return done(null, user);
+//   },
+//   async (user, done) => {
+//     const token = await rememberMeToken();
+//     await User.updateOne({ email: user.email }, { $set: { rememberMeToken: token } });
+//     return done(null, token);
+//   }
+// ));
+
+// module.exports = passport;
