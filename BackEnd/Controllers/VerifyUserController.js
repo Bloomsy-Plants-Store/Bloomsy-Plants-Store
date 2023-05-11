273d90ae67919c18f45057b4d4e7ba35262f5f1f
Radwa99 <radwa.prof.9@gmail.com>
2023-05-12 02:19:45 +0300

    handle expired or invalid verification token

--- BackEnd/Controllers/VerifyUserController.js
+++ BackEnd/Controllers/VerifyUserController.js
@@ -41,22 +41,28 @@ var prepareConfirmationMail = (name, email, confirmationCode) => {
 };
   
 
-  var verifyUser = async (req, res, next) => {
-      userModel.findOne({
-        confirmationCode: req.params.confirmationCode 
+var verifyUser = async (req, res, next) => {
+    userModel.findOne({
+      confirmationCode: req.params.confirmationCode 
     })
-      .then((user) => {
-        if (!user) {
-          return res.status(404).send({ message: "User Not found." });
-        }
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: "User Not found." });
+      }
+  
+      try {
+        jwt.verify(req.params.confirmationCode, config.SECRETKEY); // Verify the token isn't expired
         user.status = "Active";
-        return user.save(); 
-      })
-      .then(()=>{
-        return res.status(200).json({ message: "User Verified successfully." });
-      })
-      .catch((e) => console.log("error", e));
-  }; 
+        return user.save();
+      } catch (error) {
+        return res.status(401).json({ message: "Invalid or expired token." });
+      }
+    })
+    .then(() => {
+      return res.status(200).json({ message: "User Verified successfully." });
+    })
+    .catch((e) => res.status(401).json({ message: "Error occurs while verification." }));
+};
 
 
 module.exports = {verifyUser,sendVerificationEmail,prepareConfirmationMail,setVerificationToken};
