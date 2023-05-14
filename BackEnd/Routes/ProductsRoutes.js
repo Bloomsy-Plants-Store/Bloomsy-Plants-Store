17577e7e3df002aeb56d4ad3071092297292b1c2
Hager Abd El Galil <Hager.galil66@gmail.com>
2023-05-14 14:46:52 +0300

    Delete Product Api

--- BackEnd/Routes/ProductsRoutes.js
+++ BackEnd/Routes/ProductsRoutes.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const ProductController = require('../controllers/ProductController');
+const ProductController = require('../Controllers/ProductController');
 const upload = require('../MiddleWares/MutlerUpload');
 
 // mutler upload
 router.post('/upload', upload.single('jsonFile') ,ProductController.uploadProducts);
 
 
-// get all products
+// Get All Products
 router.get('/', ProductController.getAllProducts);
 router.get('/bestSelling', ProductController.getBestSellingProducts);
 router.get('/topRating', ProductController.getTopRatingProducts);
 
+// Delete Product 
+router.delete("/:id", ProductController.deleteProduct);
 
 module.exports = router;
 
