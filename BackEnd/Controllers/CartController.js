4c8380368a02317958c07fa2745fcbf3dc644c7d
Radwa99 <radwa.prof.9@gmail.com>
2023-05-16 11:53:54 +0300

    edit cart controller

--- BackEnd/Controllers/CartController.js
+++ BackEnd/Controllers/CartController.js
@@ -73,10 +73,87 @@ var updateCartItems = async (req, res) => {
       return res.status(500).json({ error: 'Server Error, Failed to update the cart' });
     }
 };
-  
+
+
+
+
 
 module.exports = {
     updateCart,
     updateCartItemById,
     updateCartItems
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// this is in case using produt_id instead of cart_id
+
+// var updateCartItemByProductId = async (req, res) => {
+//   try {
+//     const user = await User.findById(req.params.id);
+//     if (!user) {
+//       return res.status(404).json({ error: 'User not found' });
+//     }
+//     const cartItemIndex = user.cart.findIndex((item) => item.product_id.toString() === req.params.cartItemId);
+//     if (cartItemIndex === -1) {
+//       return res.status(404).json({ error: 'Cart item not found in cart' });
+//     }
+
+//     if (req.body.quantity) {
+//       user.cart[cartItemIndex].quantity = req.body.quantity;
+//     }
+    
+//     console.log(user.cart[cartItemIndex].product_id);
+//     await user.save();
+//     console.log(user.cart);
+//     return res.status(200).json({ message: 'Cart item quantity updated successfully' });
+//   } catch (error) {
+//     console.log(error)
+//     return res.status(500).json({ error: 'Server Error, Failed to update the cart' });
+//   }
+// };
+
+
+
+
+
+// var updateCartItemsbyProductIds = async (req, res) => {
+//   try {
+//     const updates = req.body.products_updates; 
+//     const user = await User.findById(req.params.id);
+//     if (!user) {
+//       return res.status(404).json({ error: 'User not found' });
+//     }
+//     for (const update of updates) {
+//       const { cartItemId, quantity, productId } = update;
+//       const cartItemIndex = user.cart.findIndex((item) => item.product_id.toString() === cartItemId);
+
+//       if (cartItemIndex === -1) {
+//         return res.status(404).json({ error: `Cart item with ID ${cartItemId} not found` });
+//       }
+//       if (quantity !== undefined) {
+//         user.cart[cartItemIndex].quantity = quantity;
+//       }
+//     }
+//     await user.save();
+//     console.log(user.cart);
+//     return res.status(200).json({ message: 'Cart items updated successfully' });
+//   } catch (error) {
+//     return res.status(500).json({ error: 'Server Error, Failed to update the cart' });
+//   }
+// };
