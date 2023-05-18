57b403217e02d6bee51717d2eb415ea68f0f8021
Radwa99 <radwa.prof.9@gmail.com>
2023-05-18 09:07:38 +0300

    product details start

---
+++ FrontEnd/src/app/Components/Pages/product-details/product-details.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent {
+  quantity: number = 1;
+  isFavorited: boolean = false;
+
+  decreaseQuantity(): void {
+    if (this.quantity > 1) {
+      this.quantity--;
+    }
+  }
+
+  increaseQuantity(): void {
+    this.quantity++;
+  }
+
+  toggleFavorite(): void {
+    this.isFavorited = !this.isFavorited;
+  }
+}
--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -25,6 +25,7 @@ import { LoginComponent } from './Components/Pages/Login_Register/login/login.co
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ResetPasswordComponent } from './Components/Pages/Login_Register/reset-password/reset-password.component';
 import { CarouselModule } from 'ngx-owl-carousel-o';
+import { ProductDetailsComponent } from './Components/Pages/product-details/product-details.component';
 
 @NgModule({
   declarations: [
@@ -47,7 +48,8 @@ import { CarouselModule } from 'ngx-owl-carousel-o';
     AboutUsHeaderComponent,
     ALlProductsComponent,
     AllProductsHeaderComponent,
-    ResetPasswordComponent
+    ResetPasswordComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
