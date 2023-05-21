24b5ae6f568250325b50481e2db6a516a51e13d1
rowan <rowantamer5@gmail.com>
2023-05-21 03:58:29 +0300

    add to cart page

---
+++ FrontEnd/src/app/Components/Pages/cart/cart-details/cart-details.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+
+
+
+export interface Product {
+  name: string;
+  price: number;
+  quantity: number;
+  total: number;
+}
+
+@Component({
+  selector: 'app-cart-details',
+  templateUrl: './cart-details.component.html',
+  styleUrls: ['./cart-details.component.css']
+})
+export class CartDetailsComponent {
+
+  displayedColumns: string[] = ['image', 'name', 'price', 'quantity', 'total'];
+  dataSource: Product[] = [
+    { name: 'Product 1', price: 10, quantity: 2, total: 20 },
+  ];
+}
