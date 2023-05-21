670e642387561da70fe25e83344d4f64b4b13fc0
rowan <rowantamer5@gmail.com>
2023-05-21 23:17:36 +0300

    add to cart

--- FrontEnd/src/app/Services/cart.service.ts
+++ FrontEnd/src/app/Services/cart.service.ts
@@ -8,6 +8,13 @@ export class CartService {
 private Base_URL = "http://localhost:7400/users/"
   constructor(private readonly myClient: HttpClient) { }
 
+
+  // add product to cart
+  // /:id/cart/add
+  addProductToCart(id: Number, product_id: Number) {
+    return this.myClient.post(this.Base_URL + id + "/cart/add", { product_id });
+  }
+
   // get all products in cart
   GetAllProductsInCart(id:Number) {
     return this.myClient.get(this.Base_URL+id+"/cart")
