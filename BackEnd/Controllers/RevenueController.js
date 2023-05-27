4812653ed1feecd6c2b1663cec42cef6ec8a9684
Radwa99 <radwa.prof.9@gmail.com>
2023-05-27 20:13:39 +0300

    edit controlller

--- BackEnd/Controllers/RevenueController.js
+++ BackEnd/Controllers/RevenueController.js
@@ -49,9 +49,8 @@ const countTotalRevenue = async (req, res) => {
 
 
   const getTotalUserCount = async (req, res) => {
-    try {
-      const totalUserCount = await User.countDocuments();
-      res.status(200).json({ users: totalUserCount });
+    try {  
+      res.status(200).json({ users: User.countDocuments() });
     } catch (error) {
       console.error('Error:', error);
       res.status(500).json({ error: 'Internal server error' });
