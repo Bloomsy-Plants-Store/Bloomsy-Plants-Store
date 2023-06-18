da63af5e0739e322efe1f96192d88e9baf691696
rowan <rowantamer5@gmail.com>
2023-06-18 19:45:04 +0300

    tracking order

--- FrontEnd/src/app/Services/order.service.ts
+++ FrontEnd/src/app/Services/order.service.ts
@@ -9,6 +9,9 @@ export class OrderService {
 
   private Base_URL = `${config.backendUrl}/user/order/`;
   http: any;
+  // public orderUpdatedSubject: Subject<void> = new Subject<void>();
+  // public orderUpdated: Observable<void> = this.orderUpdatedSubject.asObservable();
+
 
   constructor(private readonly myClient : HttpClient) { }
 
@@ -19,6 +22,17 @@ export class OrderService {
   makeOrder(userID: number, total_price: number, products: any) {
     console.log(userID,total_price,products)
     return this.myClient.post(this.Base_URL + userID, { total_price, products });
+  }
+
+  confirmOrder(orderID: number,userID: number) {
+    return this.myClient.post(this.Base_URL + "confirm-order" , {orderID,userID});
+  }
+  deliverOrder(orderID: number,userID: number) {
+    return this.myClient.post(this.Base_URL + "deliver-order" , {orderID,userID});
+  }
+
+  getOrders() {
+    return this.myClient.get<any[]>(this.Base_URL + 'get-pending-orders');
   }
 
 }
